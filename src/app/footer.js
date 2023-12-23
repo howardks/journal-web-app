c89369ecc0b695c94d4619a8ee744c9058e5d597
howardks <93079698+howardks@users.noreply.github.com>
2023-12-23 15:25:57 -0500

    Restart

--- src/app/footer.js
+++
@@ -1,9 +0,0 @@
-export default function Footer() {
-    const currentYear = new Date().getFullYear();
-
-    return (
-        <div className="footer">
-            <p>&copy; {currentYear} Kaylie Howard</p>
-        </div>
-    );
-}
--- src/app/header.js
+++
@@ -1,7 +0,0 @@
-export default function Header() {
-    return (
-        <div className="header">
-            <p>Web Journal</p>
-        </div>
-    );
-}
